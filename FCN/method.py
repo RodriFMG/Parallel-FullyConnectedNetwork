6f98a7044c4f806095fc3818166ddf9163bdfdd3
RODRIGO <rodrigo.meza@utec.edu.pe>
2025-10-30 08:16:17 -0500

    Sequential FCN <- Aún no ejecuta

---
+++ FCN/method.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+def to_batch(data, label, batch_size=64, shuffle=True):
+    if not isinstance(data, np.ndarray):
+        data = np.array(data).astype(np.float32)
+    if not isinstance(label, np.ndarray):
+        label = np.array(label).astype(np.int64)
+
+    if data.shape[0] != label.shape[0]:
+        raise ValueError(f"Cantidad dispareja de muestras y labels: \n"
+                         f"Data: {data.shape[0]}\n"
+                         f"Labels: {label.shape[0]}")
+
+    total_data = data.shape[0]
+
+    if shuffle:
+        indexs = np.arange(total_data)
+        np.random.shuffle(indexs)
+        data = data[indexs]
+        label = label[indexs]
+
+    return [
+        [data[step:step + batch_size], label[step:step + batch_size]]
+        for step in range(0, total_data, batch_size)
+    ]
+
+
+def LossFunction(z, label):
+    N = label.shape[0]
+    pred = z[np.arange(N), label]
+    return -1 / N * np.sum(np.log(pred + 1e-8))
+
+
+def GradLossFunction(z, label):
+    N = label.shape[0]
+    grad = z.copy()
+    grad[np.arange(N), label] -= 1
+
+    # Como el gradiante lleva -1/N, arrastra ese 1/N al derivar...
+    grad /= N
+    return grad
+
+
+def accuracy(model, eval_data):
+    N = 0
+    Corrects = 0
+
+    for data, label in eval_data:
+        z = model.forward(data)
+        pred = np.argmax(z, axis=1)
+        Corrects += np.sum(pred == label)
+        N += label.shape[0]
+
+    return float(Corrects) / N
+
+
+# Se supone que train_data ya debe estar batcherizado (xd)
+def train(model, train_data, eval_data, epochs=100, show=10, lr=0.001):
+    avg_loss = None
+    factor_loss = 0.1
+    for epoch in range(epochs):
+        for data, label in train_data:
+            z = model.forward(data)
+            loss = LossFunction(z, label=label)
+            z_grad = GradLossFunction(z, label=label)
+            model.backward(z_grad, lr=lr)
+
+            if avg_loss is None:
+                avg_loss = loss
+            else:
+                avg_loss = factor_loss * loss + (1- factor_loss) * avg_loss
+
+        if (epoch + 1) % show == 0:
+            acc = accuracy(model, eval_data)
+            print(f"Epoca: {epoch} --> avg_loss: {avg_loss:.5f}, accuracy: {acc:.5f}")
---
+++ FCN/model.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+
+def softmax(x):
+    div = np.sum(np.exp(x)) + 1e-8
+    return np.exp(x) / div
+
+
+# Para paralelizarlo, todos los np.dot tenemos que cambiarlos por el producto matricial (o suma)
+# en su version paralela
+class Linear:
+    def __init__(self, in_, out_):
+        self.input = None
+        self.output = None
+        self.weights = np.random.randn(in_, out_).astype(np.float32)
+        self.bias = np.zeros(out_).astype(np.float32)
+
+    def forward(self, x):
+        self.input = x
+        self.output = np.dot(x, self.weights) + self.bias
+
+        return self.output
+
+    def backward(self, z, lr=0.001):
+        grad_input = np.dot(z, self.weights.T)  # dL/dx = dL/dz * dz/dx = z * w
+        grad_weights = np.dot(self.input.T, z)  # dL/dw = dL/dz * dz/dw = z * x
+        grad_bias = np.sum(z, axis=0, keepdims=True)  # dL/db = dL/dz * dz/db = 1
+
+        self.weights -= lr * grad_weights
+        self.bias -= lr * grad_bias
+
+        return grad_input
+
+
+class ReLU:
+    def __init__(self):
+        self.input = None
+
+    def forward(self, x):
+        self.input = x
+
+        xp = x.copy()
+        xp[xp < 0] = 0
+        return xp
+
+    def backward(self, z):
+        grad = np.where(self.input > 0, z, 0)
+        return grad
+
+
+class FullyConnected:
+
+    def __init__(self, in_, out_, num_capas=5):
+
+        self.num_capas = num_capas
+        middle = (num_capas - 2) // 2
+
+        increm = in_
+        factor = lambda val, i: val * 2 if middle < i else val // 2
+
+        for i in range(num_capas - 1):
+            past_increm = int(increm)
+            increm = int(factor(increm, i))
+            capa_i = Linear(past_increm, increm)
+            setattr(self, f"capa_{i}", capa_i)
+
+        self.capa_f = Linear(increm, out_)
+        self.ReLU = ReLU()
+
+    def forward(self, x):
+
+        if not isinstance(x, np.ndarray):
+            x = np.array(x).astype(np.float32)
+
+        for i in range(self.num_capas - 1):
+            capa_i = getattr(self, f"capa_{i}")
+            x = capa_i.forward(x)
+            x = self.ReLU.forward(x)
+
+        last_capa = self.capa_f.forward(x)
+
+        return softmax(last_capa)
+
+    def backward(self, z, lr=0.001):
+
+        grad = self.capa_f.backward(z=z, lr=lr)
+
+        for i in range(self.num_capas - 1, 0, -1):
+            capa_i = getattr(self, f"capa_{i}")
+            grad = self.ReLU.backward(z=grad)
+            grad = capa_i.backward(z=grad, lr=0.001)
